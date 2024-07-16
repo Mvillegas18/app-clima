e02f06c3198b0ab566ec733b42f527f14f7cc6c6
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-16 12:50:44 -0500

    style: Update App component with title and container styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
+import styles from './App.module.css';
+import Form from './components/Form/Form';
+
 export default function App() {
-	return <div>App</div>;
+	return (
+		<>
+			<h1 className={styles.title}>App</h1>
+
+			<div className={styles.container}>
+				<Form />
+				<p>2</p>
+			</div>
+		</>
+	);
 }
 
