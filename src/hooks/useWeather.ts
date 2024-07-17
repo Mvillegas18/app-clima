8ae1dcc6dd4dfe9b094752ecbeaefeea6a187aff
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-17 09:42:51 -0500

    feat: Add fetchWeather function to useWeather hook

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,7 +1,22 @@
-export default await function useWeather() {
-	const fetchWeather = async () => {};
+import axios from 'axios';
+import { SearchType } from '../types';
+
+export default function useWeather() {
+	const fetchWeather = async (search: SearchType) => {
+		try {
+			const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${
+				search.city
+			},${search.country}&appid=${import.meta.env.VITE_API_KEY}`;
+
+			const { data } = await axios(geoUrl);
+
+			console.log(data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
 	return {
 		fetchWeather,
 	};
-};
+}
